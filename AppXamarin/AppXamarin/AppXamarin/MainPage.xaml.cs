5d6555d9a963c1ed9b55253ccb04e44ee28784e9
Bia <fabianazs@gmail.com>
2019-10-16 11:40:24 -0300

    First commit

---
+++ AppXamarin/AppXamarin/AppXamarin/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using AppXamarin.Servico.Modelo;
+using AppXamarin.Servico;
+
+namespace AppXamarin
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            botao.Clicked += buscarCEP;
+        }
+
+        private void buscarCEP(object sender, EventArgs args)
+        {
+            //TODO - Lógica do programa
+            //TODO - Validações
+
+            string Cep = cep.Text.Trim();
+            Endereco end = ViaCEPServico.BuscarEnderecoViaCEP(Cep);
+
+            resultado.Text = string.Format("Endereço: {0} - {1} \n {2}, {3}", end.logradouro, end.bairro, end.localidade, end.uf );
+
+        }
+    }
+}
---
+++ AppXamarin/AppXamarin/AppXamarin/Servico/Modelo/ViaCEPServico.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using AppXamarin.Servico.Modelo;
+using Newtonsoft.Json;
+
+namespace AppXamarin.Servico.Modelo
+{
+    public class ViaCEPServico
+    {
+        private static string EnderecoURL = "http://viacep.com.br/ws/{0}/json/";
+        public static Endereco BuscarEnderecoViaCEP(string cep)
+        {
+            string novoEnderecoURL = string.Format(EnderecoURL, cep);
+
+            WebClient wc = new WebClient();
+            string conteudo = wc.DownloadString(novoEnderecoURL);
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(conteudo);
+
+            return end;
+            
+        }
+    }
+}
